c849d446c1b4039593a8a513bef5b46ac6031325
jsssofficial <67999970+jsmsj@users.noreply.github.com>
2022-03-16 21:03:06 +0530

    code cleanup and one modification

--- _helpers/_functions.py
+++ _helpers/_functions.py
@@ -10,139 +10,125 @@ from requests.structures import CaseInsensitiveDict
 
 ia = Cinemagoer()
 
-async def make_request(url):
-    return requests.get(url)
 
-def getIdFromUrl(link: str):
-    if len(link) in [33, 19]:
-        return link
-    if "folders" in link or "file" in link:
-        regex = r"https://drive\.google\.com/(drive)?/?u?/?\d?/?(mobile)?/?(file)?(folders)?/?d?/(?P<id>[-\w]+)[?+]?/?(w+)?"
-        res = re.search(regex,link)
-        if res is None:
-            raise IndexError("GDrive ID not found.")
-        return res.group('id')
-    parsed = urlparse.urlparse(link)
-    return parse_qs(parsed.query)['id'][0]
-
-
-async def file_size(id):
-    url = "https://drive.google.com/uc?id="+str(id)+"&export=download"
-    x = await make_request(url)
-    return1=bs4.BeautifulSoup(x.text, 'html.parser').find('div', id = "uc-text")
-    web_page = return1.find('a')
-    len1=len(str(web_page)[77:-4])
-    size=str(return1)[len1+241:-291].replace("G"," GB").replace("M"," MB").replace("T"," TB")
-    if "</span> is too large for" in size:
-        size = size[:-94]
-    if size == "":
-        size = "User rate limit exceeded. Unable to get size"
-    return size
-
-async def name_of_file(url):
-    if "uc?id=" in url:
-        i_d = getIdFromUrl(url)
-        url = "https://drive.google.com/file/d/" + i_d + "/view"
-    req = await make_request(url)
-    soup = bs4.BeautifulSoup(req.text, "html.parser")
-    name = str(soup.title).replace("<title>","").replace("</title>","")
-    if name == "Meet Google Drive – One place for all your files":
-        return "Error"
-    name = name[:-15]
-    return name
-
-async def send_name(url):
-    initial = await name_of_file(url)
-    if initial == "Error":
-        name = "Can't get name"
-    else:
-        name = initial
-
-    name = name.replace("."," ")
-    return name
-
-def make_url(source):
-    if "https://" in source or "http://" in source:
-        return source
-    else:
-        if source.startswith("drive.google.com"):
-            sour = "https://" + source
-            return sour 
+class GoogleDriveObject():
+    def __init__(self,link,size=None):
+        self.link = link
+        if size:
+            self.size = size
         else:
-            sour = "http://drive.google.com/open?id=" + source
-            return sour
+            self.size = None
 
-def get_id(url):
-    try:
-        source = getIdFromUrl(url)
-    except IndexError:
-        return f"Source id not found in {url}"
-    return source
+    def getIdFromUrl(self):
+        if len(self.link) in [33, 19]:
+            return self.link
+        if "folders" in self.link or "file" in self.link:
+            regex = r"https://drive\.google\.com/(drive)?/?u?/?\d?/?(mobile)?/?(file)?(folders)?/?d?/(?P<id>[-\w]+)[?+]?/?(w+)?"
+            res = re.search(regex,self.link)
+            if res is None:
+                raise IndexError("GDrive ID not found.")
+            return res.group('id')
+        parsed = urlparse.urlparse(self.link)
+        return parse_qs(parsed.query)['id'][0]
 
-def parse_name(name):
-    return PTN.parse(name)
-
-def imdb_name(name):
-    info_dict = parse_name(name)
-    try:
-        title = info_dict["title"]
-    except:
-        title=""
-    try:
-        year = info_dict["year"]
-    except:
-        year=""
-    final_name = str(title) + " " + str(year)
-    if final_name == "": return "Cannot find the title and year of the link provided"
-    return final_name
-
-def imdb_movie_list(name):
-    return ia.search_movie(name)
-
-def movie_title_with_year(movie):
-    try:
-        name =  movie['title'] + " " + str(movie['year'])
-    except:
-        name = movie['title']
-    return name
-
-def movie_id(movie):
-    return movie.movieID
-
-def movie_obj(movie_id):
-    return ia.get_movie(str(movie_id))
-
-def get_genre_list(imdb_movie_object):
-    return imdb_movie_object.get('genres')
-
-async def give_size(drive_url):
-    url = "http://gdrivesize.jsmsj.repl.co/checksize"
-
-    headers = CaseInsensitiveDict()
-    headers["Content-Type"] = "application/json"
-
-    data = "{" + '"link"' + ":" + f'"{drive_url}"' + "}"
-
-    
-    try:
-        resp = requests.post(url, headers=headers, data=data)
-        return resp.json()['size']
-    except:
+    def full_name(self):
         try:
-            return await file_size(getIdFromUrl(drive_url))
+            query = "http://drive.google.com/open?id=" + self.getIdFromUrl()
+        except IndexError:
+            return "Please send Google Drive link in some other format."
+        req = requests.get(query)
+        soup = bs4.BeautifulSoup(req.text, "html.parser")
+        name = str(soup.title).replace("<title>","").replace("</title>","")
+        if name == "Meet Google Drive – One place for all your files":
+            raise ValueError("Unable to get name")
+        name = name[:-15]
+        return name
+
+    def name(self):
+        try:
+            initial_name = self.full_name()
+        except IndexError:
+            return "Please send Google Drive link in some other format."
+        except ValueError:
+            return "Unable to get name"
+        final_name = initial_name.replace("."," ")
+        return final_name
+
+    def parsed_name(self):
+        return PTN.parse(self.full_name())
+
+    def main_name(self):
+        info_dict = self.parsed_name()
+        try:
+            title = info_dict["title"]
         except:
-            return "Unable to get size"
+            title=""
+        try:
+            year = info_dict["year"]
+        except:
+            year=""
+        final_name = str(title) + " " + str(year)
+        if final_name == "": raise ValueError("Cannot find the title and year of the link provided")
+        return final_name
 
+    def file_size_from_id(self):
+        url = "https://drive.google.com/uc?id="+str(self.getIdFromUrl)+"&export=download"
+        x = requests.get(url)
+        return1=bs4.BeautifulSoup(x.text, 'html.parser').find('div', id = "uc-text")
+        web_page = return1.find('a')
+        len1=len(str(web_page)[77:-4])
+        size=str(return1)[len1+241:-291].replace("G"," GB").replace("M"," MB").replace("T"," TB")
+        if "</span> is too large for" in size:
+            size = size[:-94]
+        if size == "":
+            raise ValueError("User rate limit exceeded. Unable to get size")
+        return size
 
-def brute_imdb_link(name):
-    query = name+ " site:imdb.com"
-    pool = search(query, tld="com",num=1,stop=1,pause=2)
-    for i in pool:
-        return i
+    def give_size(self):
+        if self.size: return self.size
+        url = "http://gdrivesize.jsmsj.repl.co/checksize"
+        headers = CaseInsensitiveDict()
+        headers["Content-Type"] = "application/json"
+        data = "{" + '"link"' + ":" + f'"{self.link}"' + "}"
+        try:
+            resp = requests.post(url, headers=headers, data=data)
+            return resp.json()['size']
+        except:
+            try:
+                return self.file_size_from_id()
+            except:
+                raise ValueError("Unable to get size")
 
-def id_from_imdblink(url):
-    pattern = re.compile(r"https?://(www\.)?imdb\.com/title/tt(\d+)/")
-    matches = pattern.finditer(url)
-    for match in matches:
-        return match.group(2)
+class ImdbObject():
+    def id_from_imdblink(self,url):
+        pattern = re.compile(r"https?://(www\.)?imdb\.com/title/tt(\d+)/?")
+        matches = pattern.finditer(url)
+        for match in matches:
+            return str(match.group(2))
+
+    def brute_imdb_link(self,name):
+        query = name+ " site:imdb.com"
+        pool = search(query, tld="com",num=1,stop=1,pause=2)
+        for i in pool:
+            return i
+    
+    def imdb_movie_list(self,name):
+        return ia.search_movie(name)
+
+    def movie_title_with_year(self,movie):
+        try:
+            name =  movie['title'] + " " + str(movie['year'])
+        except:
+            name = movie['title']
+        return name
+
+    def movie_id(self,movie):
+        return movie.movieID
+
+    def movie_obj(self,movie_id):
+        return ia.get_movie(str(movie_id))
+
+    def get_genre_list(self,imdb_movie_object):
+        return imdb_movie_object.get('genres')
+
 
--- main.py
+++ main.py
@@ -4,118 +4,131 @@ from _helpers._functions import *
 import discord
 from discord.ext import commands
 
-
 intents = discord.Intents.default()
 
 bot = commands.Bot(command_prefix=secrets.prefix, intents=intents,help_command=None)
-
 @bot.event
 async def on_ready():
     await bot.change_presence(activity=discord.Game(name=f"{secrets.prefix}info"))
     print("Bot is ready!")
 
-@bot.event
-async def on_message(message):
-    if message.author.bot:return
-    await bot.process_commands(message)
+"""
+`genre1 | genre2 | genre3 | genre4 | genre5`
+:drive: Name with spaces
+
+> Size: XX GB
+> Link: https://drive.google.com/xxxxx
+
+https://imdb.com/title/ttxxxxxx
+"""
 
 @bot.command()
-async def template(ctx,drive_link=None,imdb_link=None,actual_size=None):
-    if not drive_link: return await ctx.send(f"Please enter the google drive link! . Run `{secrets.prefix}info template` for syntax.")
+@commands.max_concurrency(1,per=commands.BucketType.default,wait=False)
+async def template(ctx,google_drive_link=None,actual_imdb_link=None,actual_size=None):
+    if not google_drive_link: return await ctx.send(f"Please provide Google Drive Link. Run `{secrets.prefix}info template` for syntax.")
     async with ctx.typing():
-        if not imdb_link: 
-            pretty_name = await send_name(drive_link)
-            search_name = imdb_name(pretty_name)
-            if search_name == "Cannot find the title and year of the link provided":return await ctx.send(f"Please provide imdb link as well. Run `{secrets.prefix}info template` for syntax.")
-            try:
-                list_of_movies = imdb_movie_list(search_name)
-            except:
-                return await ctx.send(f"Please provide imdb link as well. Run `{secrets.prefix}info template` for syntax.")
-            if len(list_of_movies) == 0 :
+        drive_obj = GoogleDriveObject(google_drive_link,actual_size)
+        imdb_obj = ImdbObject()
+        if not actual_imdb_link:
+            async with ctx.typing():
                 try:
-                    linkk = brute_imdb_link(search_name)
-                    if linkk:
-                        mov_id = id_from_imdblink(brute_imdb_link(search_name)).replace(" ","")
-                    else:
-                        return await ctx.send(f"Please provide imdb link as well. Run `{secrets.prefix}info template` for syntax.")
-                    if mov_id == "": return await ctx.send(f"Please provide imdb link as well. Run `{secrets.prefix}info template` for syntax.")
+                    search_name_for_imdb = drive_obj.main_name()
+                except ValueError:
+                    return await ctx.send(f"Please provide imdb link as well. Run `{secrets.prefix}info template` for syntax.")
+                try:
+                    list_of_movies = imdb_obj.imdb_movie_list(search_name_for_imdb)
                 except:
                     return await ctx.send(f"Please provide imdb link as well. Run `{secrets.prefix}info template` for syntax.")
-                imdb_id = mov_id
-            else:
-                new_movie_ls = []
-                i=1
-                for movie in list_of_movies:
-                    new_movie_ls.append(f"{i}. {movie_title_with_year(movie)}")
-                    i+=1
-                temp = '\n'.join(new_movie_ls)
-                movie_list_msg = await ctx.send(f"```\n{temp}\n```")
-                await ctx.send("Choose the index number of the name of the series/movie\nEg: reply 2 for the second value\nIf your movie/show is not in the list reply none")
-                def check(m):
-                    return m.author == ctx.author
-                msg = await bot.wait_for('message', check=check)
-                try:
-                    index = int(msg.content) - 1
-                except:
-                    if msg.content.lower() == "none":
-                        linkk = brute_imdb_link(search_name)
-                        if linkk:
-                            mov_id = id_from_imdblink(brute_imdb_link(search_name)).replace(" ","")
-                        else:
-                            return await ctx.send(f"Please provide imdb link as well. Run `{secrets.prefix}info template` for syntax.")
-                        if mov_id == "":
-                            return await ctx.send(f"Please provide imdb link as well. Run `{secrets.prefix}info template` for syntax.")
-                        else:
-                            imdb_id = mov_id
-                    else:
-                        return await ctx.send("You have to enter an integer for the index number !!")
-                try:
+                if len(list_of_movies) == 0 :
                     try:
-                        final_movie = list_of_movies[index]
+                        linkk = imdb_obj.brute_imdb_link(search_name_for_imdb)
+                        if linkk:
+                            mov_id = imdb_obj.id_from_imdblink(imdb_obj.brute_imdb_link(search_name_for_imdb)).replace(" ","")
+                        else:
+                            return await ctx.send(f"Please provide imdb link as well. Run `{secrets.prefix}info template` for syntax.")
+                        if mov_id == "": return await ctx.send(f"Please provide imdb link as well. Run `{secrets.prefix}info template` for syntax.")
                     except:
-                        if not msg.content.lower() == "none": return await movie_list_msg.reply("You have entered the index number which is not in this list")
-                    imdb_id = movie_id(final_movie)
-                except:
-                    pass
-            async with ctx.typing():
-                list_of_genres = get_genre_list(movie_obj(imdb_id))
-                if len(list_of_genres) ==0: list_of_genres = ['No Genre Found']
-                name_string = pretty_name
-                genre_string = f"`{' | '.join(list_of_genres)}`"
-                if actual_size:
-                    size_string = f"> Size: **{str(actual_size)}**"
+                        return await ctx.send(f"Please provide imdb link as well. Run `{secrets.prefix}info template` for syntax.")
+                    imdb_id = mov_id
                 else:
-                    size = await give_size(drive_link)
-                    size_string = f"> Size: **{size}**"
-                link_string = f"> Link: <{drive_link}>"
+                    new_movie_ls = []
+                    i=1
+                    for movie in list_of_movies:
+                        new_movie_ls.append(f"{i}. {imdb_obj.movie_title_with_year(movie)}")
+                        i+=1
+                    temp = '\n'.join(new_movie_ls)[-1989::]
+                    movie_list_msg = await ctx.send(f"```\n{temp}\n```")
+                    await ctx.send("Choose the index number of the name of the series/movie\nEg: reply 2 for the second value\nIf your movie/show is not in the list reply none")
+                    def check(m):
+                        return m.author == ctx.author
+                    msg = await bot.wait_for('message', check=check)
+                    try:
+                        index = int(msg.content) - 1
+                    except:
+                        if msg.content.lower() == "none":
+                            linkk = imdb_obj.brute_imdb_link(search_name_for_imdb)
+                            if linkk:
+                                mov_id = imdb_obj.id_from_imdblink(imdb_obj.brute_imdb_link(search_name_for_imdb)).replace(" ","")
+                            else:
+                                return await ctx.send(f"Please provide imdb link as well. Run `{secrets.prefix}info template` for syntax.")
+                            if mov_id == "":
+                                return await ctx.send(f"Please provide imdb link as well. Run `{secrets.prefix}info template` for syntax.")
+                            else:
+                                imdb_id = mov_id
+                        else:
+                            return await ctx.send("You have to enter an integer for the index number !!")
+                    try:
+                        try:
+                            final_movie = list_of_movies[index]
+                        except:
+                            if not msg.content.lower() == "none": return await movie_list_msg.reply("You have entered the index number which is not in this list")
+                        imdb_id = imdb_obj.movie_id(final_movie)
+                    except:
+                        pass
+            async with ctx.typing():
+                list_of_genres = imdb_obj.get_genre_list(imdb_obj.movie_obj(imdb_id))
+                if len(list_of_genres) ==0: list_of_genres = ['No Genre Found']
+                try:
+                    name_string = drive_obj.name()
+                except:
+                    name_string = "Unable to get name"
+                genre_string = f"`{' | '.join(list_of_genres)}`"
+                try:
+                    sizeee = drive_obj.give_size()
+                except ValueError:
+                    sizeee = "Unable to get size"
+                size_string = f"> Size: **{sizeee}**"
+                link_string = f"> Link: <{google_drive_link}>"
                 imdb_link_string = f"https://www.imdb.com/title/tt{imdb_id}"
 
                 ultimate = genre_string + "\n" + ":drive: " + name_string + "\n\n" + size_string + "\n" + link_string + "\n\n" + imdb_link_string
-
                 await ctx.send(f"Preview:\n\n{ultimate}")
                 await ctx.send(f"Copy paste this:\n```\n{ultimate}\n```")
-                return
+                return    
         else:
             async with ctx.typing():
-                pretty_name = await send_name(drive_link)
-                mov_id = id_from_imdblink(imdb_link)
-                list_of_genres = get_genre_list(movie_obj(mov_id))
+                mov_id = imdb_obj.id_from_imdblink(actual_imdb_link)
+                list_of_genres = imdb_obj.get_genre_list(imdb_obj.movie_obj(mov_id))
                 if len(list_of_genres) ==0: list_of_genres = ['No Genre Found']
-
-                name_string = pretty_name
+                name_string = drive_obj.name()
                 genre_string = f"`{' | '.join(list_of_genres)}`"
-                if actual_size:
-                    size_string = f"> Size: **{str(actual_size)}**"
-                else:
-                    size = await give_size(drive_link)
-                    size_string = f"> Size: **{size}**"
-                link_string = f"> Link: <{drive_link}>"
-                imdb_link_string = imdb_link
+                try:
+                    sizeee = drive_obj.give_size()
+                except ValueError:
+                    sizeee = "Unable to get size"
+                size_string = f"> Size: **{sizeee}**"
+                link_string = f"> Link: <{google_drive_link}>"
+                imdb_link_string = actual_imdb_link
 
                 ultimate = genre_string + "\n" + ":drive: " + name_string + "\n\n" + size_string + "\n" + link_string + "\n\n" + imdb_link_string
 
                 await ctx.send(f"Preview:\n\n{ultimate}")
                 await ctx.send(f"Copy paste this:\n```\n{ultimate}\n```")
+
+@template.error
+async def temp_error(ctx,error):
+    if isinstance(error, commands.MaxConcurrencyReached):
+        return await ctx.reply('An instance of this command is already running. Please wait for it to finish.')
 
 @bot.group(invoke_without_command=True)
 async def info(ctx):
@@ -133,6 +146,5 @@ async def template(ctx):
     em.add_field(name="Note:",value="The size which is automatically detected by the bot does not happens for folders for now.",inline=False)
     em.set_footer(text="Parameters in <> are required, while in [] are optional")
     await ctx.send(embed=em)
-
 
 bot.run(secrets.bot_token)
